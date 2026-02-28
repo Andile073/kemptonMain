b93f94ac6177e37fcb9a80093ede2cd88ba04fe0
Andile Mayo <160361993+Andile073@users.noreply.github.com>
2026-02-28 15:43:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-// Navbar background on scroll
-window.addEventListener("scroll", function() {
-    const navbar = document.querySelector(".navbar");
-
-    if (window.scrollY > 50) {
-        navbar.style.background = "rgba(15,23,42,0.95)";
-        navbar.style.boxShadow = "0 5px 20px rgba(0,0,0,0.4)";
-    } else {
-        navbar.style.background = "rgba(15,23,42,0.6)";
-        navbar.style.boxShadow = "none";
-    }
-});
-
-// Mobile Menu
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("navLinks");
 
 hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+    navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
 });
